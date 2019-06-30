e171f2d4c1be6bc43df5e2968c45f4d2010dbb1a
Juan Heredia <juan.heredia@bigfinite.com>
2019-06-30 23:38:37 +0200

    fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ const IndexPage = () => (
   <Layout>
     <h1>I am Juan Heredia, developer with the soul of an entrepreneur and a frontend enthusiast.</h1>
     <p>After having founded my own company and working as a freelance in all stages of long projects I have been given the ability and passion to develop any project from sketches to scalability and final performance. Previously teaching the basics of IA and machine learning at the UAB and now developing and leading the frontend team at Bigfinite Inc.</p>
-    <p>currently based in Barcelona.</p>
-    <p>write me by <a href='mailto:jherediagu@gmail.com'>email</a> or you can even follow me on <a href='https://www.linkedin.com/in/juanantonioheredia/'>LinkedIn</a>, <a href='https://twitter.com/jherediagu'>Twitter</a> or <a href='https://github.com/jherediagu/'>GitHub</a>.</p>
+    <p>Currently based in Barcelona.</p>
+    <p>Write me by <a href='mailto:jherediagu@gmail.com'>email</a> or you can even follow me on <a href='https://www.linkedin.com/in/juanantonioheredia/'>LinkedIn</a>, <a href='https://twitter.com/jherediagu'>Twitter</a> or <a href='https://github.com/jherediagu/'>GitHub</a>.</p>
   </Layout>
 )
 
