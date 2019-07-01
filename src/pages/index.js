22dffacabeaa56563b9a587e0b18b0bde3b9de19
Juan Heredia <juan.heredia@bigfinite.com>
2019-07-01 08:20:03 +0200

    fix text

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import Layout from '../components/layout';
 const IndexPage = () => (
   <Layout>
     <h1>I am Juan Heredia, developer with the soul of an entrepreneur and a frontend enthusiast.</h1>
-    <p>After having founded my own company and working as a freelance in all stages of long projects I have been given the ability and passion to develop any project from sketches to scalability and final performance. Previously teaching the basics of IA and machine learning at the UAB and now developing and leading the frontend team at Bigfinite Inc.</p>
+    <p>Having founded my own company and worked as a freelance in all stages of a product has given me the skill and passion to develop any project from sketches to scalability and final performance. </p> 
+    <p>Previously taught the basis of IA and Machine Learning at the UAB, now developing and leading the frontend team at Bigfinite Inc.</p>
     <p>Currently based in Barcelona.</p>
     <p>Write me by <a href='mailto:jherediagu@gmail.com'>email</a> or you can even follow me on <a href='https://www.linkedin.com/in/juanantonioheredia/'>LinkedIn</a>, <a href='https://twitter.com/jherediagu'>Twitter</a> or <a href='https://github.com/jherediagu/'>GitHub</a>.</p>
   </Layout>
