8beeb864ef6f788e531cbdbd320a6d4e65d221b1
Juan Heredia <juan.heredia@bigfinite.com>
2019-12-27 09:20:27 +0100

    Updates

---
+++ component---src-pages-404-js-5a112790b7136db63842.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement("h1",null,"What are you looking for?"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5a112790b7136db63842.js.map
